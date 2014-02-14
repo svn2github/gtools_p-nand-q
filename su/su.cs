5b900b9c1dc70a933235f5bb5ae49af98f75b9bc
gerson.kurz@gmail.com <gerson.kurz@gmail.com@7a4b3922-6d1b-6526-e715-f50a7420fd4d>
2014-02-14 08:07:10 +0000

    Added 'su' command line tool
    git-svn-id: http://pserv4.googlecode.com/svn/trunk@26 7a4b3922-6d1b-6526-e715-f50a7420fd4d

---
+++ su/su.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Principal;
+using System.Diagnostics;
+using System.IO;
+
+namespace su
+{
+    class su
+    {
+        private static bool IsAdministrator()
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        static void Main(string[] args)
+        {
+            if( !IsAdministrator() )
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo(Path.Combine(Environment.SystemDirectory, "cmd.exe"));
+                startInfo.Verb = "runas";
+
+                startInfo.WorkingDirectory = Environment.CurrentDirectory;
+                startInfo.Arguments = string.Format("/K \"cd /d {0}\"", startInfo.WorkingDirectory);
+
+                System.Diagnostics.Process.Start(startInfo);
+            }
+        }
+    }
+}
