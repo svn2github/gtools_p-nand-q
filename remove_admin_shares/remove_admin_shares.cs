c397953c24a68e97291ceefbc0bfb266c198265e
gerson.kurz@gmail.com <gerson.kurz@gmail.com@7a4b3922-6d1b-6526-e715-f50a7420fd4d>
2014-02-14 13:30:15 +0000

    Added 'remove_admin_shares' command line tool
    git-svn-id: http://pserv4.googlecode.com/svn/trunk@31 7a4b3922-6d1b-6526-e715-f50a7420fd4d

---
+++ remove_admin_shares/remove_admin_shares.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management;
+
+namespace remove_admin_shares
+{
+    class remove_admin_shares
+    {
+        static void Main(string[] args)
+        {
+            int sharesToDelete = 0;
+
+            try
+            {
+                var searcher = new ManagementObjectSearcher(
+                    "root\\CIMV2",
+                    "SELECT * FROM Win32_Share");
+
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    ++sharesToDelete;
+                    string shareName = queryObj["Name"].ToString();
+                    Console.WriteLine(resource.IDS_DeleteingShare, shareName, queryObj["Path"]);
+                    try
+                    {
+                        queryObj.Delete();
+                    }
+                    catch (ManagementException ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                        Console.WriteLine(resource.IDS_ERR_UnableToDeleteShare, shareName);
+                    }
+                }
+            }
+            catch (ManagementException ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine(resource.IDS_ERR_UnableToQueryWMIData);
+            }
+            if (sharesToDelete != 0)
+            {
+                Console.WriteLine(resource.IDS_delete_summary, sharesToDelete);
+            }
+            else
+            {
+                Console.WriteLine(resource.IDS_no_share_to_delete);
+            }
+        }
+    }
+}
